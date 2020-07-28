5137773e57b9714a2bf87b3f7198ae04f95d16fd
Dima Hnatovskyi <dmtrvld@gmail.com>
2020-07-28 14:27:58 +0300

    Implement gracefull shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"github.com/st0rrer/datahow-service/src/log"
 	"github.com/st0rrer/datahow-service/src/metrics"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
 	"time"
 )
 
@@ -56,9 +60,34 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	err = metricsSrv.ListenAndServe()
-	if err != nil {
-		logger.WithError(err).Fatalln("could not start metrics server")
-	}
+	go func() {
+		err := metricsSrv.ListenAndServe()
+		if err != nil {
+			logger.WithError(err).Fatalln("could not start metrics server")
+		}
+	}()
 
+
+	signalCh := make(chan os.Signal)
+	signal.Notify(signalCh, os.Interrupt)
+
+	<-signalCh
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
+
+	go shutDownSrv(&wg, &metricsSrv)
+	go shutDownSrv(&wg, &apiSrv)
+
+	wg.Wait()
+	logger.Infoln("server is successfully shutdown")
+}
+
+func shutDownSrv(wg *sync.WaitGroup, srv *http.Server) {
+
+	defer wg.Done()
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+	srv.Shutdown(ctx)
 }
